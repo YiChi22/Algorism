1c44b421b6788eea56d7a8f546e9e64a25080455
yichi <coding2210@gmail.com>
2022-11-16 12:36:13 +0900

    Update Level0_옹알이.py
    이전 코드가 잘 못 짜여짐 다른 이의 코드를 보고 변경

--- programmers/Level0_옹알이.py
+++ programmers/Level0_옹알이.py
@@ -1,15 +1,15 @@
+from itertools import combinations, permutations
 word = ["aya", "ye", "woo", "ma"]
 
 
 def solution(babbling):
     answer = 0
-    if 1 <= len(babbling) <= 100:
-        for i in range(len(babbling)):
-            if len(babbling[i]) > 16:
-                continue
-            for j in range(len(word)):
-                if babbling[i].find(word[j]) > 0:
-                    answer += 1
-                    break
+    words = []
+    for i in range(2, len(word) + 1):
+        for j in list(combinations(word, i)):
+            words += {''.join(s) for s in permutations(j)}
+    for i in babbling:
+        if i in word + words:
+            answer += 1
 
     return answer
